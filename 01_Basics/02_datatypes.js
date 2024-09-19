c2f105fd0627ac222e5ca6571292c0e3262e3ef7
Shruti Pandey <135129821+Shruti14Pandey@users.noreply.github.com>
2024-09-19 10:13:14 +0000

    datatypes in JS

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //this instruct the engine that - treat all JS code as newer version
+
+// alert(3+3)  //this is node.js , not browser
+
+// code readability should be high, write prettier code
+
+let name = "Shruti"
+let age = 19
+let isLoggedin = false
+
+// number - 2 to power 53
+// bigint
+// String - you can either use single or double quotes but i would  prefer double  ""
+// boolean = true/false
+// null = standalone value 
+// undefined = it means that the value isn't defined yet, however null signifies that there is an empty value
+// symbol = unique
+
+// object
+
+console.log(typeof age) // this gives the type of variable
+console.log(typeof null) // output - object
+console.log(typeof undefined) // output - undefined
