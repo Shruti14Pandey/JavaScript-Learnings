1fedb98c1acfac6c0f439a21e70a7279100a2cf2
Shruti Pandey <135129821+Shruti14Pandey@users.noreply.github.com>
2024-09-19 17:54:42 +0000

    summary of types

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// JavaScript is dynamically typed, 
+// which means that the interpreter assigns a variable's type at runtime based on the value of the variable
+
+
+//Primitives - they support call by value,  that is a copy is sent
+
+//7 : String, Number , Boolean , null , undefined , symbol , BigInt
+
+//Refence(non-primitives) 
+
+//Arrays , Objects and functions
+
+const heros = ["ironman" , "hulk" , "Thor"]
+
+let obj = {
+    name : "Smriti",
+    age : 30,
+}
+
+const myFunction = function(){
+    console.log("Hello World!!")
+}
