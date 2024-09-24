aff4882dfdba0df7c514ebedbcdb544ed218722e
Shruti Pandey <135129821+Shruti14Pandey@users.noreply.github.com>
2024-09-24 15:08:01 +0000

    iife update

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -4,7 +4,9 @@
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); //do not forget the semicolon
+
+// At times there arises problem in context to global scopes, inorder to skip such problems we use iife 
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
