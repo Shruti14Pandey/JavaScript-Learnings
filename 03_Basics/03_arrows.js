4f5280ff5afcbf627016da7ad97063ddec1a2c1e
Shruti Pandey <135129821+Shruti14Pandey@users.noreply.github.com>
2024-09-24 14:31:57 +0000

    this and arrow

--- 03_Basics/03_arrows.js
+++ 03_Basics/03_arrows.js
@@ -35,15 +35,15 @@ const chai =  () => {
 
 // chai()
 
-// const addTwo = (num1, num2) => {
+// const addTwo = (num1, num2) => { //EXPLICIT RETURN
 //     return num1 + num2
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2 //arrow functions used without paranthesis and return is termed as IMPLICIT RETURN
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"}) //inorder to return object , you need to wrap it in paranthesis else it will give undefined
 
 
 console.log(addTwo(3, 4))
@@ -52,3 +52,7 @@ console.log(addTwo(3, 4))
 // const myArray = [2, 5, 3, 7, 8]
 
 // myArray.forEach()
+
+// The global object in browser (console.log(this))is termed as Window. However, in nodeJS it is {}
+
+// if this keyword is used in a function without an object , then it will return undefined
