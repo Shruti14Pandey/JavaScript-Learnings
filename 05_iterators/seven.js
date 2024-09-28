19cd208d0ca3427b89e9e4899f53939ee6e7eedf
Shruti Pandey <135129821+Shruti14Pandey@users.noreply.github.com>
2024-09-28 16:28:26 +0000

    loops discussion

--- 05_iterators/seven.js
+++ 05_iterators/seven.js
@@ -2,8 +2,8 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
-const newNums = myNumers
-                .map((num) => num * 10 )
+const newNums = myNumers                              //the map function also waits for callbacks, and it automatically returns value
+                .map((num) => num * 10 )               //here this is map chaining 
                 .map( (num) => num + 1)
                 .filter( (num) => num >= 40)
 
